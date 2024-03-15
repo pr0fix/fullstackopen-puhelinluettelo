513a36ecbf712e7a1b4cbc687fc55a578fd736f8
pr0fix <bgx152@myy.haaga-helia.fi>
2024-03-15 19:14:56 +0200

    osa3 tehtava 3.13

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config()
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require("./models/person");
 const app = express();
 
 app.use(cors());
@@ -19,102 +21,97 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(express.static('dist'));
 
 // initialize phonebook
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
+// let persons = [
+//     {
+//         id: 1,
+//         name: "Arto Hellas",
+//         number: "040-123456"
+//     },
+//     {
+//         id: 2,
+//         name: "Ada Lovelace",
+//         number: "39-44-5323523"
+//     },
+//     {
+//         id: 3,
+//         name: "Dan Abramov",
+//         number: "12-43-234345"
 
-    },
-    {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
+//     },
+//     {
+//         id: 4,
+//         name: "Mary Poppendieck",
+//         number: "39-23-6423122"
 
-    }
-];
+//     }
+// ];
 
 // get all persons
 app.get("/api/persons", (request, response) => {
-    response.json(persons);
+    Person.find({}).then(persons => {
+        response.json(persons);
+    })
 });
 
 // get person by id
 app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(person => person.id === id);
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    };
-});
+    const id = request.params.id;
+    Person.findById(id).then(person => {
+        if(person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+        
+    });
 
 // delete person
 app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter(person => person.id !== id);
-    response.status(204).end();
+    const id = request.params.id;
+    Person.findByIdAndDelete(id).then(() => {
+        response.status(204).end()
+    })
 });
-
-// generate id for new person
-const generatePersonId = () => {
-    const personId = Math.floor(Math.random() * 5000);
-    return personId;
-}
 
 // add person
 app.post("/api/persons", (request, response) => {
-    const body = request.body
-    if (!body.name) {
+    const body = request.body;
+
+    if (!body.name || !body.number) {
         return response.status(400).json({
-            error: 'name information missing'
-        });
-    } else if (!body.number) {
-        return response.status(400).json({
-            error: 'number information missing'
+            error: 'name or number information missing'
         });
     }
 
-    if (persons.some(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        });
-    } else {
-        const person = {
-            id: generatePersonId(),
+    Person.findOne({ name: body.name }).then(existingPerson => {
+        if (existingPerson) {
+            return response.status(400).json({
+                error: 'name must be unique'
+            });
+        }
+
+        const person = new Person({
             name: body.name,
             number: body.number
-        };
+        });
 
-        persons = persons.concat(person);
-
-        response.json(person);
-    };
-
+        person.save().then(savedPerson => {
+            response.json(savedPerson);
+        });
+    });
 });
 
 // show info page
 app.get("/info", (request, response) => {
-    const timestamp = new Date();
-    response.send(
-        `Phonebook has info for ${persons.length} people <br/>
-        ${timestamp}`
-    );
+    Person.countDocuments().then(count => {
+        const timestamp = new Date();
+        response.send(`Phonebook has info for ${count} people <br/> ${timestamp}`)
+    })
 });
 
 // define and configure server port
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
