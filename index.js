7def2fd09c1fd24d8f8655ab6320378bc095d7c5
pr0fix <bgx152@myy.haaga-helia.fi>
2024-03-21 10:07:22 +0200

    osa3 tehtava 3.16

--- index.js
+++ index.js
@@ -20,6 +20,19 @@ morgan.token('body', request => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(express.static('dist'));
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if(error.name === "CastError") {
+        return response.status(400).send({error: "malformatted id"});
+    }
+    next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'});
+};
+
 // initialize phonebook
 // let persons = [
 //     {
@@ -46,32 +59,35 @@ app.use(express.static('dist'));
 //     }
 // ];
 
+
+
 // get all persons
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (request, response, next) => {
     Person.find({}).then(persons => {
         response.json(persons);
     })
+    .catch(error => next(error));
 });
 
 // get person by id
-app.get("/api/persons/:id", (request, response) => {
-    const id = request.params.id;
-    Person.findById(id).then(person => {
+app.get("/api/persons/:id", (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
         if(person) {
-            response.json(person)
+            response.json(person);
         } else {
-            response.status(404).end()
-        }
+            response.status(404).end();
+        };
     })
+    .catch(error => next(error));
         
     });
 
 // delete person
-app.delete("/api/persons/:id", (request, response) => {
-    const id = request.params.id;
-    Person.findByIdAndDelete(id).then(() => {
-        response.status(204).end()
+app.delete("/api/persons/:id", (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id).then(() => {
+        response.status(204).end();
     })
+    .catch(error => next(error));
 });
 
 // add person
@@ -82,7 +98,7 @@ app.post("/api/persons", (request, response) => {
         return response.status(400).json({
             error: 'name or number information missing'
         });
-    }
+    };
 
     Person.findOne({ name: body.name }).then(existingPerson => {
         if (existingPerson) {
@@ -99,16 +115,23 @@ app.post("/api/persons", (request, response) => {
         person.save().then(savedPerson => {
             response.json(savedPerson);
         });
+        
     });
+    
 });
 
 // show info page
-app.get("/info", (request, response) => {
+app.get("/info", (request, response, next) => {
     Person.countDocuments().then(count => {
         const timestamp = new Date();
-        response.send(`Phonebook has info for ${count} people <br/> ${timestamp}`)
+        response.send(`Phonebook has info for ${count} people <br/> ${timestamp}`);
     })
+    .catch(error => next(error));
 });
+
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 // define and configure server port
 const PORT = process.env.PORT
